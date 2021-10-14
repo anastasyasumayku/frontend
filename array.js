a9e2f80928e2e8d22c06ba5d90b644bbdb7a1a7b
anastasyasumayku <92319564+anastasyasumayku@users.noreply.github.com>
2021-10-14 08:16:32 +0700

    Add files via upload

---
+++ array.js
@@ -0,0 +1,27 @@
+
+var studentsData = [
+	{
+		id: 1,
+		NIM: 105021910024,
+		fullName: 'Anastasya Alicia Sumayku',
+		gender: 'Female',
+		fakultas: 'FIK',
+		study: 'Informatika',
+	},
+	{
+		id: 2,
+		NIM: 105012345678,
+		fullName: 'Michael George',
+		gender: 'Male',
+		fakultas: 'FKIP',
+		study: 'Pendidikan Agama',
+	},
+	{
+		id: 3,
+		NIM: 105087654321,
+		fullName: 'Alie Britney',
+		gender: 'Female',
+		fakultas: 'FIK',
+		study: 'Sistem Informasi',
+	}
+]
